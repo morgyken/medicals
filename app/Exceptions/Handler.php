88fc1fa484cb08bfc56320a25884407d02b1177d
Samuel Odhiambo <sodhiambo@collabmed.com>
2016-09-08 13:34:42 +0300

    Configure platform

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -62,4 +62,20 @@ class Handler extends ExceptionHandler
 
         return redirect()->guest('login');
     }
+    /**
+     * Render an exception using Whoops.
+     *
+     * @param  \Exception $e
+     * @return \Illuminate\Http\Response
+     */
+    protected function renderExceptionWithWhoops(Exception $e)
+    {
+        $whoops = new \Whoops\Run;
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+        return new \Illuminate\Http\Response(
+            $whoops->handleException($e),
+            $e->getStatusCode(),
+            $e->getHeaders()
+        );
+    }
 }
